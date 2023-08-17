d948fb624fc7ae5cc417f89a7c99206711ddbe06
Jatin Foujdar <jatin321001@gmail.com>
2023-08-17 17:32:10 +0530

    option commit

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -32,56 +32,52 @@ export const CarouselImages =[
 ];
 
 
-  // <img src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_850,h_504/rng/md/carousel/production/5c5330b7210697c6dca6303647c64827" class="h-[344px] object-cover py-10 px-6 " alt="Image 1" style="transition: transform 0.5s ease 0s; transform: scale(1);"></img>
 
+export const Option =[
+  {
+    id: '1',
+    url: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/v1674029856/PC_Creative%20refresh/3D_bau/banners_new/Pizza.png"
 
+  },
+  {
+    id: '2',
+    url: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/v1674029845/PC_Creative%20refresh/3D_bau/banners_new/Burger.png"
 
-  // const [currentIndex, setCurrentIndex] = useState(0);
-  // console.log(props.data.length)
+  },
+  {
+    id: '3',
+    url: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/v1674029848/PC_Creative%20refresh/3D_bau/banners_new/Chinese.png"
 
-  // const prevImage = () => {
-  //   setCurrentIndex(currentIndex - 1);
-  // };
+  },
+  {
+    id: '4',
+    url: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/v1674029858/PC_Creative%20refresh/3D_bau/banners_new/Rolls.png"
 
-  // const nextImage = () => {
-  //   setCurrentIndex(currentIndex + 1);
-  // };
+  },
+  {
+    id: "5",
+    url: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/v1674029858/PC_Creative%20refresh/3D_bau/banners_new/Pure_Veg.png"
+  },
+  {
+    id: "6",
+    url: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/v1674029860/PC_Creative%20refresh/3D_bau/banners_new/Sandwich.png"
+  },
+  {
+    id: "7",
+    url: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/v1674029847/PC_Creative%20refresh/3D_bau/banners_new/Kachori.png"
+  },
+  {
+    id: "8",
+    url: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/v1675667625/PC_Creative%20refresh/Biryani_2.png"
+  },
+  {
+    id: "9",
+    url: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/v1674029845/PC_Creative%20refresh/3D_bau/banners_new/Cakes.png"
+  },
+  {
+    id: "10",
+    url: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/rng/md/carousel/production/4f8570eb368254b261407644be5b4184"
+  },
 
-  // return (
-  //   <div className="relative">
-  //     {currentIndex!=0 && <button
-  //       className="absolute z-10 top-1/2 left-2 bg-white text-gray-900 rounded-full p-4 transform -translate-y-1/2 hover:shadow-md focus:outline-none transition-all duration-500 ease-in-out"
-  //       onClick={prevImage}
-  //     >
-  //       <AiOutlineArrowLeft className="h-6 w-6 hover:-translate-x-2 transition-transform duration-750  ease-in-out" />
-  //     </button>}
-
-  //     {currentIndex +3!=props.data.length && <button
-  //       className="absolute z-10 top-1/2 right-2 bg-white text-gray-900 rounded-full p-4 transform -translate-y-1/2 hover:shadow-md focus:outline-none"
-  //       onClick={nextImage}
-  //     >
-  //       <AiOutlineArrowRight className="h-6 w-6 hover:translate-x-2 transition-transform duration-750 ease-in-out" />
-  //     </button>}
-
-  //     <span
-  //       className="flex  text-center transition-all duration-1000 ease-in-out "
-  //       style={{ transform: `translateX(-${currentIndex * 24}rem)` }}
-  //     >
-  //       {props?.data?.map((image, index) => (
-  //         <img
-  //           key={index}
-  //           src={`https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_520,h_520/${image.data.creativeId}`}
-  //           className={`h-[344px] object-cover py-10 px-6 ${
-  //             index === 0 ? "ml-0" : ""
-  //           }`}
-  //           alt={`Image ${index}`}
-  //           style={{ transition: "transform .5s" }}
-  //     onMouseOver={(e) => (e.currentTarget.style.transform = "scale(1.05)")}
-  //     onMouseOut={(e) => (e.currentTarget.style.transform = "scale(1)")}
-  //         />
-  //       ))}
-  //     </span>
-  //   </div>
-
- 
+];
 
