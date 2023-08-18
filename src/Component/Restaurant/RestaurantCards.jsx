8fcf226a39c405996df00f74e6c9b6a265c2a580
Jatin Foujdar <jatin321001@gmail.com>
2023-08-18 12:49:22 +0530

    body enjected commit

--- src/Component/Restaurant/RestaurantCards.jsx
+++ src/Component/Restaurant/RestaurantCards.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { restaurantList,IMG_CDN_URL } from '../../MockData/restaurantList';
 
 const RestaurantCard = () => {
+  const {name,cloudinaryImageId, avgRating, cuisines,link  }= restaurantList[0].data
   return (
     <div className="w-[290px] relative py-6 mx-3 ml-24 ">
         
@@ -9,17 +10,17 @@ const RestaurantCard = () => {
      
         <img
           className="w-full h-[180px] object-cover rounded-md"
-          src={IMG_CDN_URL+restaurantList[0].data?.cloudinaryImageId }
-          alt={restaurantList[0].data?.name}
+          src={IMG_CDN_URL+cloudinaryImageId }
+          alt={name}
         />
-        <h4 className="pt-2 font-semibold  mr-1">⭐{restaurantList[0].data?.avgRating}</h4>
-        <h1 className="pt-2 font-semibold text-xl">{restaurantList[0].data?.name}</h1>
+        <h4 className="pt-2 font-semibold  mr-1">⭐{avgRating}</h4>
+        <h1 className="pt-2 font-semibold text-xl">{name}</h1>
         <h6 className="font-normal text-gray-600 text-xs">
-          {restaurantList[0].data?.cuisines?.join(", ")}
+          {cuisines?.join(", ")}
         </h6>
         <a
-          className="mt-2 text-blue-500 hover:underline"
-          href={restaurantList[0].data?.link}
+          className="mt-2 text-blue-500 cursor-pointer hover:underline"
+          href={link}
           target="_blank"
           rel="noopener noreferrer"
         >
