c2393f58ab163d5c6d3d554df310bf78dfc849f2
Jatin Foujdar <jatin321001@gmail.com>
2023-09-09 21:53:07 +0530

    commit

--- src/Component/Body.js
+++ src/Component/Body.js
@@ -21,29 +21,29 @@ const Body = () => {
   },[]);
   
         async function getRestaurants() {
-        // handle the error using try... catch
+        
         try {
           const response = await fetch("https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING");
           const json = await response.json();
     
-          // initialize checkJsonData() function to check Swiggy Restaurant data
+          
           async function checkJsonData(jsonData) {
             for (let i = 0; i < jsonData?.data?.cards.length; i++) {
     
-              // initialize checkData for Swiggy Restaurant data
+           
               let checkData = json?.data?.cards[i]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
     
-              // if checkData is not undefined then return it
+              
               if (checkData !== undefined) {
                 return checkData;
               }
             }
           }
     
-          // call the checkJsonData() function which return Swiggy Restaurant data
+    
           const resData = await checkJsonData(json);
     
-          // update the state variable restaurants with Swiggy API data
+          
           setFilteredRestaurants(resData);
         
         } catch (error) {
