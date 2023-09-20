d1cb41aa11652a8cce68a0304e3ebf49aa7fdcec
Jatin Foujdar <jatin321001@gmail.com>
2023-09-20 16:33:38 +0530

    menu commit

--- src/Component/Restaurant/RestaurantMenu.jsx
+++ src/Component/Restaurant/RestaurantMenu.jsx
@@ -1,20 +1,58 @@
-import React, { useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+import {
+  swiggy_menu_api_URL,
+  IMG_CDN_URL,
+  ITEM_IMG_CDN_URL,
+  MENU_ITEM_TYPE_KEY,
+  RESTAURANT_TYPE_KEY,
+} from "../../MockData/restaurantList";
 
 const RestaurantMenu = () => {
     const {id} = useParams();
+    const [restauraunt,   setRestaurant] = useState({})
 
     useEffect(()=>{
       getRestaurantInfo();
     },[])
-
-    async function getRestaurantInfo(){
-      
+    async function getRestaurantInfo() {
+      try {
+        const response = await fetch(swiggy_menu_api_URL + resId);
+        const json = await response.json();
+  
+        // Set restaurant data
+        const restaurantData = json?.data?.cards?.map(x => x.card)?.
+                               find(x => x && x.card['@type'] === RESTAURANT_TYPE_KEY)?.card?.info || null;
+        setRestaurant(restaurantData);
+  
+        // Set menu item data
+        const menuItemsData = json?.data?.cards.find(x=> x.groupedCard)?.
+                              groupedCard?.cardGroupMap?.REGULAR?.
+                              cards?.map(x => x.card?.card)?.
+                              filter(x=> x['@type'] == MENU_ITEM_TYPE_KEY)?.
+                              map(x=> x.itemCards).flat().map(x=> x.card?.info) || [];
+        
+        const uniqueMenuItems = [];
+        menuItemsData.forEach((item) => {
+          if (!uniqueMenuItems.find(x => x.id === item.id)) {
+            uniqueMenuItems.push(item);
+          }
+        })
+        setMenuItems(uniqueMenuItems);
+      } catch (error) {
+        setMenuItems([]);
+        setRestaurant(null);
+        console.log(error);
+      }
     }
     
   return (
-    <div>RestaurantMenu:{id}</div>
+    <div>
+      <h2>RestaurantMenu:{id} </h2>
+      <h2>{restauraunt.name}</h2>
+      </div>
   )
-}
+  }
+
 
 export default RestaurantMenu
--- src/MockData/restaurantList.js
+++ src/MockData/restaurantList.js
@@ -1,4 +1,4 @@
-export const IMG_CDN_URL= "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/"
+// export const IMG_CDN_URL= "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/"
 
 export const FETCHAPI = "https://www.swiggy.com/dapi/menu/v4/full?lat=12.9351929&lng=77.62448069999999&menuId="
 
@@ -7,7 +7,7 @@ export const Restaurant = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=2
 export const CorsRestaurant = "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING"
 
 // Image CDN URL for Restaurant card
-export const IMG_CDN_URLs =
+export const IMG_CDN_URL =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 
 // Image CDN URL for Restaurant Menu
@@ -21,12 +21,6 @@ export const swiggy_api_URL =
 // Swiggy API to get Restaurant Menu data with corsproxy
 export const swiggy_menu_api_URL =
 "https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=21.1702401&lng=72.83106070000001&&submitAction=ENTER&restaurantId=";
-
-// shimmer card unit
-export const shimmer_card_unit = 20;
-
-// shimmer Menu card unit
-export const shimmer_menu_card_unit = 4;
 
 // menu items api card type key
 export const MENU_ITEM_TYPE_KEY = "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory";
